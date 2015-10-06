e6f55e94c3a299253568a655512e8cf21165d333
Natzugen <Natzugen@users.noreply.github.com>
2015-10-06 14:29:19 -0500

    Create Masterlevel.cpp

---
+++ Masterlevel.cpp
@@ -0,0 +1,5 @@
+//rf function ml skill tree
+
+void CMasterLevelSkillTreeSystem::MLS_SkillMonkBarrageJustOneTargetMastery(int aIndex,CMagicInf * lpMagic,int aTargetIndex)
+
+float fSkillValue = GetMasterSkillValue(MagicDamageC.SkillGet(lpMagic->m_Skill),lpMagic->m_Level)
