a61724d0d6631b8c240ab2deb2d7c0514a00431c
Natzugen <Natzugen@users.noreply.github.com>
2015-10-23 07:47:55 -0500

    tarkan and tantalos lizard fix

--- Eldorado.cpp
+++ Eldorado.cpp
@@ -60,3 +60,115 @@ void CEledoradoEvent::RegenGoldDercon()
                 }
                 AllSendServerMsg(szTemp);
             }
+
+
+
+///////////////
+
+tarkan tantalos and atlans lizard fix.
+
+find 
+for (n=0;n<OBJ_MAXMONSTER;n++)
+{
+MapNumber = 7;
+
+if ( gObj[n].Class == 80 ) // Devil Lizard King
+{
+gObj[n].Live = TRUE;
+gObj[n].MapNumber = MapNumber;
+
+while ( gMSetBase.GetBoxPosition(MapNumber, 50, 10, 250, 200, gObj[n].X, gObj[n].Y) == 0 )
+{
+
+}
+
+this->m_BossDevilLizardKingMapNumber = gObj[n].MapNumber;
+this->m_BossDevilLizardKingMapX = gObj[n].X;
+this->m_BossDevilLizardKingMapY = gObj[n].Y;
+
+gObj[n].Life = gObj[n].MaxLife;
+replace with
+
+for (n=0;n<OBJ_MAXMONSTER;n++)
+{
+
+
+if ( gObj[n].Class == 80 ) // Devil Lizard King
+{
+gObj[n].Live = TRUE;
+MapNumber = 7;
+gObj[n].MapNumber = MapNumber;
+
+while ( gMSetBase.GetBoxPosition(MapNumber, 50, 10, 250, 200, gObj[n].X, gObj[n].Y) == 0 )
+{
+
+}
+
+this->m_BossDevilLizardKingMapNumber = gObj[n].MapNumber;
+this->m_BossDevilLizardKingMapX = gObj[n].X;
+this->m_BossDevilLizardKingMapY = gObj[n].Y;
+
+gObj[n].Life = gObj[n].MaxLife;
+gObj[n].TX = gObj[n].X;
+gObj[n].TY = gObj[n].Y;
+gObj[n].MTX = gObj[n].X;
+gObj[n].MTY = gObj[n].Y;
+gObj[n].StartX = gObj[n].X;
+gObj[n].StartY = gObj[n].Y;
+gObj[n].m_State = 1;
+gObj[n].PathCount = 0;
+
+find
+
+MapNumber = 8;
+gObj[n].MapNumber = MapNumber;
+
+while ( gMSetBase.GetBoxPosition(MapNumber, 50, 50, 200, 200, gObj[n].X, gObj[n].Y) == 0 )
+{
+
+}
+
+this->m_BossKanturMapNumber = gObj[n].MapNumber;
+this->m_BossKanturMapX = gObj[n].X;
+this->m_BossKanturMapY = gObj[n].Y;
+
+gObj[n].Life = gObj[n].MaxLife;
+/*LogAddTD("GoldenDebug %d HP: %d", gObj[n].Class, gObj[n].Life);
+gObj[n].TX = gObj[n].X;
+gObj[n].TY = gObj[n].Y;
+gObj[n].MTX = gObj[n].X;
+gObj[n].MTY = gObj[n].Y;
+gObj[n].StartX = gObj[n].X;
+gObj[n].StartY = gObj[n].Y;
+gObjClearBuffEffect(&gObj[n], CLEAR_TYPE_DIEREGEN);
+gObj[n].m_ViewState = 0;
+gObj[n].Teleport = 0;
+gObj[n].m_State = 1;
+gObj[n].PathCount = 0;*/
+replace with 
+
+MapNumber = 8;
+gObj[n].MapNumber = MapNumber;
+
+while ( gMSetBase.GetBoxPosition(MapNumber, 50, 50, 200, 200, gObj[n].X, gObj[n].Y) == 0 )
+{
+
+}
+
+this->m_BossKanturMapNumber = gObj[n].MapNumber;
+this->m_BossKanturMapX = gObj[n].X;
+this->m_BossKanturMapY = gObj[n].Y;
+
+gObj[n].Life = gObj[n].MaxLife;
+/ /*LogAddTD("GoldenDebug %d HP: %d", gObj[n].Class, gObj[n].Life);
+gObj[n].TX = gObj[n].X;
+gObj[n].TY = gObj[n].Y;
+gObj[n].MTX = gObj[n].X;
+gObj[n].MTY = gObj[n].Y;
+gObj[n].StartX = gObj[n].X;
+gObj[n].StartY = gObj[n].Y;
+gObjClearBuffEffect(&gObj[n], CLEAR_TYPE_DIEREGEN);
+gObj[n].m_ViewState = 0;
+gObj[n].Teleport = 0;
+gObj[n].m_State = 1;
+gObj[n].PathCount = 0;
